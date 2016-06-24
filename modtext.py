2af3c11671e89baf0a4bcab07df3aa01146041dd
Brandon Mulcahy <brandon@jangler.info>
2016-06-24 12:17:29 -0400

    modtext.py: gzip data

--- modtext.py
+++ modtext.py
@@ -2,9 +2,11 @@
 
 from argparse import ArgumentParser
 from base64 import b64decode, b64encode
+from gzip import compress, decompress
 from os.path import join
 from struct import pack, unpack_from
 from sys import stderr
+
 
 FLAG_SAMPLE_ASSOCIATED = 1 << 0
 FLAG_16BIT = 1 << 1
@@ -44,10 +46,18 @@ def dump_sample(data, offset, smpdir, smp_num):
         if smp_flags & FLAG_STEREO:
             smp_length *= 2
         smp_point = unpack_from('=I', data, offset+0x48)[0]
-        with open(join(smpdir, smp_name + '.raw'), 'wb') as raw:
-            raw.write(data[smp_point:smp_point+smp_length])
+        with open(join(smpdir, smp_name + '.sgz'), 'wb') as sgz:
+            sgz.write(compress(data[smp_point:smp_point+smp_length]))
     elif smp_flags & FLAG_SAMPLE_ASSOCIATED:
         print('could not export nameless sample %02d' % smp_num, file=stderr)
+
+
+def decode(data):
+    return decompress(b64decode(data))
+
+
+def encode(data):
+    return b64encode(compress(data))
 
 
 def module_to_text(inpath, outpath, smpdir):
@@ -65,20 +75,20 @@ def module_to_text(inpath, outpath, smpdir):
 
     with open(outpath, 'w') as f:
         f.write('[Header]\n')
-        f.write(b64encode(data[:0xc0]).decode() + '\n')
+        f.write(encode(data[:0xc0]).decode() + '\n')
         f.write('[Orders]\n')
-        f.write(b64encode(data[0xc0:0xc0+ordnum]).decode() + '\n')
+        f.write(encode(data[0xc0:0xc0+ordnum]).decode() + '\n')
         for i, offset in enumerate(ins_offsets):
             f.write('[Instrument%02d]\n' % (i+1))
-            f.write(b64encode(data[offset:offset+554]).decode() + '\n')
+            f.write(encode(data[offset:offset+554]).decode() + '\n')
         for i, offset in enumerate(smp_offsets):
             f.write('[Sample%02d]\n' % (i+1))
             dump_sample(data, offset, smpdir, i+1)
-            f.write(b64encode(data[offset:offset+0x50]).decode() + '\n')
+            f.write(encode(data[offset:offset+0x50]).decode() + '\n')
         for i, offset in enumerate(pat_offsets):
             f.write('[Pattern%03d]\n' % i)
             length = unpack_from('=H', data, offset)[0]
-            f.write(b64encode(data[offset:offset+8+length]).decode() + '\n')
+            f.write(encode(data[offset:offset+8+length]).decode() + '\n')
 
 
 def text_to_module(inpath, outpath, smpdir):
@@ -88,7 +98,7 @@ def text_to_module(inpath, outpath, smpdir):
     if not lines or lines[0] != '[Header]':
         die('%s is not a compatible text file' % inpath)
 
-    header_data = b64decode(lines[1])
+    header_data = decode(lines[1])
     ordnum, insnum, smpnum, patnum = unpack_from('4H', header_data, 0x20)
     ins_offsets = [0xc0 + ordnum + 554*i for i in range(insnum)]
     smp_offsets = [0xc0 + ordnum + 554*insnum + 0x50*i for i in range(smpnum)]
@@ -98,7 +108,7 @@ def text_to_module(inpath, outpath, smpdir):
     while i < len(lines):
         if lines[i].startswith('[Pattern'):
             pat_offsets.append(cur_offset)
-            length = unpack_from('H', b64decode(lines[i+1]), 0)[0]
+            length = unpack_from('H', decode(lines[i+1]), 0)[0]
             cur_offset += 8 + length
         i += 1
     offsets = ins_offsets + smp_offsets + pat_offsets
@@ -122,15 +132,15 @@ def text_to_module(inpath, outpath, smpdir):
                     pos += f.write(pack('%dI' % len(offsets), *offsets))
                     wrote_offsets = True
             else:
-                data = b64decode(line)
+                data = decode(line)
                 if is_sample:
                     smp_name = unpack_from(
                         '26s', data, 0x14)[0].decode().strip('\0 ')
                     smp_flags = unpack_from('B', data, 0x12)[0]
                     if smp_name and smp_flags & FLAG_SAMPLE_ASSOCIATED:
-                        raw_name = join(smpdir, smp_name + '.raw')
-                        with open(raw_name, 'rb') as raw:
-                            smp_data = raw.read()
+                        sgz_name = join(smpdir, smp_name + '.sgz')
+                        with open(sgz_name, 'rb') as sgz:
+                            smp_data = decompress(sgz.read())
                         smp_point = pack('I', cur_offset)
                         data = data[:0x48] + smp_point + data[0x4c:]
                         cur_offset += len(smp_data)
