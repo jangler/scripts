751577f49ce7bda37385e2d2a0d6bc26449d247f
Brandon Mulcahy <brandon@jangler.info>
2016-06-23 23:54:43 -0400

    Complete/fix modtext.py import and export, minus sample data

--- modtext.py
+++ modtext.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
-from base64 import b64encode, b64decode
-from struct import unpack_from
+from base64 import b64decode, b64encode
+from struct import pack, unpack_from
 from sys import stderr
 
 
@@ -40,31 +40,67 @@ def module_to_text(inpath, outpath):
         die('%s is not an IT module' % path)
 
     ordnum, insnum, smpnum, patnum = unpack_from('4H', data, 0x20)
-    ins_offsets = unpack_from('=%dL' % insnum, data, 0xc0 + ordnum)
-    smp_offsets = unpack_from('=%dL' % patnum, data, 0xc0 + ordnum + insnum*4)
+    ins_offsets = unpack_from('=%dI' % insnum, data, 0xc0 + ordnum)
+    smp_offsets = unpack_from('=%dI' % smpnum, data, 0xc0 + ordnum + insnum*4)
     pat_offsets = \
-        unpack_from('=%dL' % patnum, data, 0xc0 + ordnum + insnum*4 + smpnum*4)
+        unpack_from('=%dI' % patnum, data, 0xc0 + ordnum + insnum*4 + smpnum*4)
 
     with open(outpath, 'w') as f:
         f.write('[Header]\n')
         f.write(b64encode(data[:0xc0]).decode() + '\n')
         f.write('[Orders]\n')
         f.write(b64encode(data[0xc0:0xc0+ordnum]).decode() + '\n')
-        f.write('[Instruments]\n')
-        for offset in ins_offsets:
+        for i, offset in enumerate(ins_offsets):
+            f.write('[Instrument%02d]\n' % (i+1))
             f.write(b64encode(data[offset:offset+554]).decode() + '\n')
-        f.write('[Samples]\n')
-        for offset in smp_offsets:
-            f.write(b64encode(data[offset:offset+0x40]).decode() + '\n')
-        f.write('[Patterns]\n')
-        for offset in pat_offsets:
+        for i, offset in enumerate(smp_offsets):
+            f.write('[Sample%02d]\n' % (i+1))
+            f.write(b64encode(data[offset:offset+0x50]).decode() + '\n')
+        for i, offset in enumerate(pat_offsets):
+            f.write('[Pattern%03d]\n' % i)
             length = unpack_from('=H', data, offset)[0]
             f.write(b64encode(data[offset:offset+8+length]).decode() + '\n')
 
 
-def text_to_module(infile, outfile):
-    # TODO
-    pass
+def text_to_module(inpath, outpath):
+    with open(inpath, 'r') as f:
+        lines = [line.strip('\n') for line in f.readlines()]
+
+    if not lines or lines[0] != '[Header]':
+        die('%s is not a compatible text file' % inpath)
+
+    header_data = b64decode(lines[1])
+    ordnum, insnum, smpnum, patnum = unpack_from('4H', header_data, 0x20)
+    ins_offsets = [0xc0 + ordnum + 554*i for i in range(insnum)]
+    smp_offsets = [0xc0 + ordnum + 554*insnum + 0x50*i for i in range(smpnum)]
+    cur_offset = 0xc0 + ordnum + 554*insnum + 0x50*smpnum
+    pat_offsets = []
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith('[Pattern'):
+            pat_offsets.append(cur_offset)
+            length = unpack_from('H', b64decode(lines[i+1]), 0)[0]
+            cur_offset += 8 + length
+        i += 1
+    offsets = ins_offsets + smp_offsets + pat_offsets
+    i = 0
+    while i < len(offsets):
+        offsets[i] += len(offsets) * 4
+        i += 1
+
+    wrote_offsets = wrote_orders = False
+    pos = 0
+    with open(outpath, 'wb') as f:
+        sections = 'Header', 'Orders', 'Instrument', 'Sample', 'Pattern'
+        for line in lines:
+            if any(line.startswith('[' + s) for s in sections):
+                if line == '[Orders]':
+                    wrote_orders = True
+                elif wrote_orders and not wrote_offsets:
+                    pos += f.write(pack('%dI' % len(offsets), *offsets))
+                    wrote_offsets = True
+            else:
+                pos += f.write(b64decode(line))
 
 
 def main():
@@ -72,7 +108,7 @@ def main():
 
     if args.infile.lower().endswith('.it'):
         module_to_text(args.infile, args.outfile)
-    elif args.outfile.lower().endswith('.txt'):
+    elif args.infile.lower().endswith('.txt'):
         text_to_module(args.infile, args.outfile)
     else:
         die('input file must have extension .it or .txt')
